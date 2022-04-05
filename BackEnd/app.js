53250b5d2720b03c46852cf332a874b28a288fca
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-04-04 20:43:31 -0500

    Routes not found

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -30,6 +30,12 @@ app.use((req, res, next) => {
 //API - SESSIONS
 app.use("/api", sessionsRoutes);
 
+//Routes not found
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
+});
+
 
 app.listen(3001, () => {
   console.log(`App running on port 3001.`)
