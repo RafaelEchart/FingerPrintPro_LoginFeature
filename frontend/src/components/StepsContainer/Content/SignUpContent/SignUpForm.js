af9a8396d876182fe0190038146719ca1354021c
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-04-05 15:31:43 -0500

    Sign Up Page Finished

--- frontend/src/components/StepsContainer/Content/SignUpContent/SignUpForm.js
+++ frontend/src/components/StepsContainer/Content/SignUpContent/SignUpForm.js
@@ -1,37 +1,84 @@
-import './style.css'
+import { useState } from "react";
+import { message } from "antd";
+import "./style.css";
 
-const SignUpForm = () => {
-    console.log("hello")
+const SignUpForm = ({ next }) => {
+  const [signUpData, setsignUpData] = useState({
+    email: "",
+    password: "",
+  });
 
+  const signUpInputHandler = (inputInfo, type) => {
+    const inputValue = inputInfo.target.value;
+    switch (type) {
+      case "email":
+        setsignUpData({ ...signUpData, email: inputValue });
+        break;
 
-    return(
-        <div class="inputWrapper">
-          <div class="form-wrapper">
-            <form class="form-class" method="post">
-              <input
-                id="nameInput"
-                required
-                type="email"
-                placeholder="Email or username"
-                maxlength="15"
-              />
-              <input
-                type="password"
-                id="scoreInput"
-                required
-                placeholder="Password"
-                
-                
-              />
-              <button type="submit" form="pwgen-form" id="submitButton">
-                Sign up
-              </button>
-             
+      case "password":
+        setsignUpData({ ...signUpData, password: inputValue });
 
-            </form>
-          </div>
-        </div>
-    )
-}
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const sendData = async () => {
+    if (signUpData.email.length && signUpData.password.length) {
+      try {
+        await fetch("http://localhost:3001/api/signup", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: signUpData.email,
+            password: signUpData.password,
+          }),
+        });
+
+        message.success("Account successfully created.");
+        next()
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      message.error("Data can not be empty.");
+    }
+  };
+
+  return (
+    <div class="inputWrapper">
+      <div class="form-wrapper">
+        <form class="form-class" method="post">
+          <input
+            id="nameInput"
+            required
+            type="email"
+            placeholder="Email or username"
+            onChange={(e) => signUpInputHandler(e, "email")}
+          />
+          <input
+            type="password"
+            id="scoreInput"
+            required
+            placeholder="Password"
+            onChange={(e) => signUpInputHandler(e, "password")}
+          />
+          <button
+            type="submit"
+            form="pwgen-form"
+            id="submitButton"
+            onClick={() => sendData()}
+          >
+            Sign up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default SignUpForm;
--- frontend/src/components/StepsContainer/Content/SignUpContent/index.js
+++ frontend/src/components/StepsContainer/Content/SignUpContent/index.js
@@ -1,7 +1,9 @@
 import "./style.css";
 import SignUpForm from './SignUpForm';
+import { ArrowLeftOutlined } from "@ant-design/icons";
 
-const SignUpContent = ({ visitorData, next, back }) => (
+
+const SignUpContent = ({ next, back }) => (
   <>
     <div className="signup_division">
       <h2 className="signup_title">
@@ -18,7 +20,11 @@ const SignUpContent = ({ visitorData, next, back }) => (
       </h2>
 
      <div className="signup_form">
-     <SignUpForm />
+     <SignUpForm next={next} />
+     <div className="signup_options">
+     <span className="signup_option" onClick={next} >¿Already have an account?</span>
+     <span className="signup_option go_back" onClick={back}> <ArrowLeftOutlined /> {" "} Go back</span>
+     </div>
      </div>
 
      
