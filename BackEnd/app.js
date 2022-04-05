9eaf8b78b8f50740b29aa456f1a4f6860fbf7874
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-04-04 20:35:19 -0500

    Initial Backend server config

---
+++ BackEnd/app.js
@@ -0,0 +1,13 @@
+// Express and bodyParser for connection
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.listen(3001, () => {
+  console.log(`App running on port 3001.`)
+})
+
