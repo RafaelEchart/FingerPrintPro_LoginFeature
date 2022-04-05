60b57c8650389323d23e73914935b8f7c40d7d2d
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-04-04 20:43:03 -0500

    CORS handler

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -13,6 +13,19 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//CORS option for the backend
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 
 //API - SESSIONS
 app.use("/api", sessionsRoutes);
