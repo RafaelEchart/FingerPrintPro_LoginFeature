5113667dd8d3c5e106c2721d0452c8d5e88faa7a
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-04-04 23:54:46 -0500

    Initial frontnd petitions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import FingerprintJS from "@fingerprintjs/fingerprintjs-pro";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [visitorData, setVisitorData] = useState({
+    visitorId: "",
+    visitorIdData: "",
+  });
+
+  const getVisitorData = async (visitorId) => {
+    console.log(visitorId)
+    let visitorDataAPI = await fetch("http://localhost:3001/api/", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        visitorId: visitorId,
+      }),
+    });
+
+    visitorDataAPI = await visitorDataAPI.json()
+    visitorDataAPI = visitorDataAPI.getVisitorInfoFingerPrintJS
+    console.log(visitorDataAPI)
+    setVisitorData({ ...visitorData, visitorIdData: visitorDataAPI });
+
+  };
+
+  const initialFunction = async () => {
+    try {
+      let visitorId = await FingerprintJS.load({ apiKey: "k4olxEPmCuFGfnxkiJf5" });
+      visitorId = await visitorId.get() 
+      visitorId = visitorId.visitorId
+  
+      setVisitorData({ ...visitorData, visitorId: visitorId });
+      await getVisitorData(visitorId);
+    }catch(err){
+      console.log(err)
+    }
+
+  };
+
+  useEffect(() => {
+    initialFunction();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
