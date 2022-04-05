7c15785c1d8e8ebda3d96cb0f61c1e8b2860b700
Rafael Echart <rafaelechartsaltos@gmail.com>
2022-04-04 21:45:21 -0500

    Verify if the email is valid

--- BackEnd/sessions/controllers/sessions-controllers.js
+++ BackEnd/sessions/controllers/sessions-controllers.js
@@ -5,7 +5,6 @@ const fetch = require("node-fetch");
 //PostgreSQL connection
 const bcrypt = require("bcryptjs");
 
-
 //POSTGRESQL CONNECTION POOL
 const Pool = require("pg").Pool;
 
@@ -17,71 +16,85 @@ const pool = new Pool({
   port: process.env.API_POSTGRESQL_PORT,
 });
 
-
 //Controllers:
 //
 // - signUpController
 // - signInController - FingerprintJS PRO feature
 
 const signUpController = async (req, res, next) => {
-    const { email, password, visitorId } = req.body;
-  
-    //First Process: Never store password in plain text
-    const hashedPass = await bcrypt.hash(password, 12);
-  
-    //Second Process: Add new user to database, check for duplicates
-    pool.query(
-      "INSERT INTO users (email, password) VALUES ($1, $2)",
-      [email, hashedPass],
-      (error, results) => {
-        if (error) {
-          if (error.code === "23505") {
-            return next(new HttpError("This email is alredy registered.", 404));
-          }
-          return next(
-            new HttpError("Error trying to INSERT user, try again.", 404)
-          );
+  const { email, password, visitorId } = req.body;
+
+  //First Process: Never store password in plain text
+  const hashedPass = await bcrypt.hash(password, 12);
+
+  //Second Process: Add new user to database, check for duplicates
+  pool.query(
+    "INSERT INTO users (email, password) VALUES ($1, $2)",
+    [email, hashedPass],
+    (error, results) => {
+      if (error) {
+        if (error.code === "23505") {
+          return next(new HttpError("This email is alredy registered.", 404));
         }
-  
-        res.status(201).json(`Account Created!`);
-      }
-    );
-};
-  
-const signInController = async (req, res, next) => {
-
-    const { email, password, visitorId } = req.body;
-    let getVisitorInfoFingerPrintJS = undefined;
-    let isNewBrowser = undefined;
-
-
-
-    //--
-    //First process: Check in the persisted data if the user has 5 failed attempts in the last 5 minutes.
-
-
-
-    //--
-    //Second process: Make use of FingerprintJS Pro to verify is this browser is new to my WebPage.
-    //A get petition to /visitoris/<visitorId>?api_key=ShKyYQhDzEc2ZEHRKp6V
-    try {
-        getVisitorInfoFingerPrintJS = await fetch(
-          `https://api.fpjs.io/visitors/${visitorId}?api_key=ShKyYQhDzEc2ZEHRKp6V`
+        return next(
+          new HttpError("Error trying to INSERT user, try again.", 404)
         );
-        getVisitorInfoFingerPrintJS = await getVisitorInfoFingerPrintJS.json();
-    } catch (err){
-        return next(new HttpError("Error in API petition", 404));
-    }
-    
-    //Conditional: Check if new browser
-    if (!getVisitorInfoFingerPrintJS.visits.length) {
-    isNewBrowser = true;
-    }
-    
+      }
 
-  
+      res.status(201).json(`Account Created!`);
+    }
+  );
 };
 
+const signInController = async (req, res, next) => {
+  const { email, password, visitorId } = req.body;
+  let getVisitorInfoFingerPrintJS = undefined;
+  let isNewBrowser = undefined;
+
+  //--
+  //First process: Check in the persisted data if the user has 5 failed attempts in the last 5 minutes.
+
+  //--
+  //Second process: Make use of FingerprintJS Pro to verify is this browser is new to my WebPage.
+  //A get petition to /visitoris/<visitorId>?api_key=ShKyYQhDzEc2ZEHRKp6V
+  try {
+    getVisitorInfoFingerPrintJS = await fetch(
+      `https://api.fpjs.io/visitors/${visitorId}?api_key=ShKyYQhDzEc2ZEHRKp6V`
+    );
+    getVisitorInfoFingerPrintJS = await getVisitorInfoFingerPrintJS.json();
+  } catch (err) {
+    return next(new HttpError("Error in API petition", 404));
+  }
+
+  //Conditional: Check if new browser
+  if (!getVisitorInfoFingerPrintJS.visits.length) {
+    isNewBrowser = true;
+  }
+
+  //--
+  //Third process: Verify the email is valid (is found on the users table registry)
+
+  //A petition postgreSQL database to verify the existence of the email address
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (!rows.length) {
+      return next(new HttpError("This email is not valid!", 404));
+    }
+
+    isEmailValid = true;
+    userData = rows[0];
+  } catch (err) {
+    return next(new HttpError("Error in postgreSQL action!", 404));
+  }
+
+
+
+
+
+};
 
 exports.signUpController = signUpController;
 exports.signInController = signInController;
